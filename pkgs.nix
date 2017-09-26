cfa08da875dc09fe548b236c5406a2a3104d3342
Anton Gushcha <ncrashed@gmail.com>
2017-09-26 10:30:23 +0300

    Integrate stack nix build

---
+++ pkgs.nix
@@ -0,0 +1,6 @@
+import ((import <nixpkgs> {}).fetchFromGitHub {
+  owner = "NixOS";
+  repo = "nixpkgs-channels";
+  rev = "96457d26dded05bcba8e9fbb9bf0255596654aab";
+  sha256  = "0qv8c60n9vyn1wsviwyxz6d0ayd1cy92jz9f59wgklss059kpzdp";
+})
