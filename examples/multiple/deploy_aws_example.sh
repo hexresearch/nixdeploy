618180d1d07a286b8088830bd8a5dde89bf27e33
Sergey Ivanov <ivanovs-4@yandex.ru>
2017-10-17 17:48:43 +0500

    micro fix

--- examples/multiple/deploy_aws_example.sh
+++ examples/multiple/deploy_aws_example.sh
@@ -1,2 +1,2 @@
 # Uses ubuntu 16.04 LTS
-nix-shell ../../default.nix --command "nixdeploy deploy --argstr \"machine1:$1\" --argstr \"machine2:$2\" --argstr \"machine2:$3\" --argstr \"sshUser:ubuntu\""
+nix-shell ../../shell.nix --command "nixdeploy deploy --argstr \"machine1:$1\" --argstr \"machine2:$2\" --argstr \"machine3:$3\" --argstr \"sshUser:ubuntu\""
--- examples/single/deploy_aws_example.sh
+++ examples/single/deploy_aws_example.sh
@@ -1,2 +1,3 @@
 # Uses ubuntu 16.04 LTS
-nix-shell ../../default.nix --command "nixdeploy deploy --argstr \"testMachineHost:$1\" --argstr \"testMachineUser:ubuntu\" ./deploy.nix --verbose"
+set -x
+nix-shell ../../shell.nix --command "nixdeploy deploy --argstr \"testMachineHost:$1\" --argstr \"testMachineUser:ubuntu\" ./deploy.nix --verbose"
